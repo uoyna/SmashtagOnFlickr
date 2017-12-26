e2b4c08aba33915db225e1f711a40f7d6b75cf5d
le yang <leputz@hotmail.com>
2017-12-26 23:17:46 +0800

    Base version

---
+++ SmashtagOnFlickr/SmashtagOnFlickr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmashtagOnFlickr
+//
+//  Created by yang le on 26/12/2017.
+//  Copyright © 2017 AnyouJiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
