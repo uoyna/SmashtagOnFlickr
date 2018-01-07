a15ccbfedae2530c08687a34aef69c2a946f4de3
uoyna <uoyna@yahoo.com>
2018-01-07 12:35:45 +0800

    integration of OauthSwift

--- SmashtagOnFlickr/SmashtagOnFlickr/FlickrTableViewController.swift
+++ SmashtagOnFlickr/SmashtagOnFlickr/FlickrTableViewController.swift
@@ -89,11 +89,8 @@ class FlickrTableViewController: UITableViewController, UITextFieldDelegate{
         
         //self.searchText = "Stanford";
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.tableView.estimatedRowHeight = self.tableView.rowHeight
+        self.tableView.rowHeight = UITableViewAutomaticDimension
     }
 
 
